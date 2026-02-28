f5b730f882af5206872d72f2c6779bae7cd539ae
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2026-02-28 14:04:45 +0100

    first structure and dependencies: installed shadcn, tailwind and tanstack query

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+    <div>
+        <h1 className="text-3xl font-bold">Hello</h1>
+        <Button>Click</Button>
+    </div>
+  )
+}
+
+export default App
